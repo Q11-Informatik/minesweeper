acf90778487c01c3d12116338dd3a61962c3e904
nimradium <nimra242001@gmail.com>
2018-06-14 12:56:18 +0200

    merge

---
+++ test_GUI_2/src/de/q11/test_gui_2/Minefield.java
@@ -0,0 +1,11 @@
+package de.q11.test_gui_2;
+
+import javax.swing.JPanel;
+
+public class Minefield {
+	
+	public static JPanel createMinefield(int maxGroesse, int mineCount) {
+		
+	}
+
+}
---
+++ test_GUI_2/src/de/q11/test_gui_2/srcTest.java
@@ -0,0 +1,5 @@
+package de.q11.test_gui_2;
+
+public class srcTest {
+	
+}

2f9c6d7fc8881688c85a59dd5f598653bd85a8e0
nimradium <nimra242001@gmail.com>
2018-06-21 12:56:45 +0200

    Mouse Listener added
    ++MSButton Mouse Listener

--- minesweeper/src/de/q11/minesweeper/MSButton.java
+++ minesweeper/src/de/q11/minesweeper/MSButton.java
@@ -1,13 +1,17 @@
 package de.q11.minesweeper;
 
 import javax.swing.*;
+
+import java.awt.Color;
 import java.awt.event.*;
 
-public class MSButton extends JButton{
+public class MSButton extends JButton implements MouseListener{
 	private boolean isMine;
 	private int number;				//number of mines around the button
 	public MSButton(boolean isMine, int number){
 		super();
+		this.setContentAreaFilled(true);
+		this.setFocusPainted(false);
 		this.isMine=isMine;
 		this.number=number;
 	}
@@ -27,5 +31,35 @@ public class MSButton extends JButton{
 	}
 	public int getNumber(){
 		return this.number;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		Color color = new Color(255,255,212,100);
+		this.setBackground(color);
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
